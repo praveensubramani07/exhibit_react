cefd3a4a1f7db8aa19765abe9e5100215cbdb366
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-08-19 18:48:10 -1000

    Create Loader.js

---
+++ src/componenet/Loader.js
@@ -0,0 +1,17 @@
+import "./profile-style.css";
+
+function Loader(){
+    return(
+        <>
+        <div className="main-loader">
+            <div className="loader">
+                
+            <img src="./images/loader.gif"></img>
+            
+            </div>
+        </div>
+
+        </>
+    );
+}
+export default Loader;
