556511c423cd0394c297240005729db4fbcb245a
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-09-05 17:41:19 +0530

    Update Loging.js

--- src/componenet/Loging.js
+++ src/componenet/Loging.js
@@ -2,22 +2,12 @@ import React, { useEffect, useState } from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import Cookies from "js-cookie";
 import jwt_decode from "jwt-decode";
+import { useHistory } from "react-router-dom";
 import Nav from "./Nav";
 
-import Newuser from "./Newuser";
-import { BrowserRouter, Routes, Route, Redirect, Link } from "react-router-dom";
-import Dashboard from "./Dashboard";
-
 export default function Loging() {
-  const [cookie, setCookie] = useState(Cookies.get("email"));
-  var [check, setCheck] = useState(null);
   const [email, setEmail] = useState("not");
-  const [profileUrl, setProfileUrl] = useState();
-  // If using js-cookie
-  const cookieValue = Cookies.get("email");
-  console.log(cookieValue, "COOKIE VQLUE");
-  if (cookieValue == null) {
-  }
+  const history = useHistory();
 
   useEffect(() => {
     const fetchdata = async () => {
@@ -28,53 +18,39 @@ export default function Loging() {
         var data = await response.json();
         console.log(data.exists);
         console.log(email, "return email");
-        setCheck(data.exists);
         if (data.exists) {
-          // If using js-cookie
-          setCookie(email);
-          Cookies.set("email", email); // 'expires' is optional, sets the expiration date
+          Cookies.set("email", email); // Set the cookie if the user exists
+          history.push("/dashboard"); // Redirect to dashboard
         }
       }
     };
     fetchdata();
-  }, [email]); //postData() add her
+  }, [email, history]);
+
+  const handleLoginSuccess = (credentialResponse) => {
+    var decoded = jwt_decode(credentialResponse.credential);
+    setEmail(decoded.email);
+    console.log("ttt", decoded.email, decoded.picture);
+  };
+
+  const handleLoginError = () => {
+    console.log("Login Failed");
+  };
 
   return (
     <>
-      {/* //to redirect to dashboard if existing user,otherwise render new User , this will work only after login is clicked */}
-      {check === null ? (
-        <p></p>
-      ) : check ? (
-        window.location.href = '/dashboard'
+      {Cookies.get("email") ? (
+        history.push("/dashboard") // Redirect to dashboard if cookie is set
       ) : (
-        <Newuser gmail={email} profile={profileUrl} />
-      )}
-
-      {/* if cookie is already set redirect to dashboard */}
-      {Cookies.get("email") !== null && Cookies.get("email") ? (
-         window.location.href = '/dashboard'
-      ) : (
-        <></>
-      )}
-
-      {/* to show login button omly when cookie is not set and not checked */}
-      {check === null  ? (
-        console.log("cheking"),        <GoogleOAuthProvider clientId="268873119322-g9kj6sj7fb8dmbs2mnj2r14gnk719md0.apps.googleusercontent.com">
-          <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              var decoded = jwt_decode(credentialResponse.credential);
-              setEmail(decoded.email);
-              console.log("ttt", decoded.email, decoded.picture);
-              var trimed = [decoded.email];
-              setProfileUrl([decoded.picture]);
-            }}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          />
-        </GoogleOAuthProvider>
-      ) : (
-        <></>
+        <div>
+          {/* Your login content */}
+          <GoogleOAuthProvider clientId="268873119322-g9kj6sj7fb8dmbs2mnj2r14gnk719md0.apps.googleusercontent.com">
+            <GoogleLogin
+              onSuccess={handleLoginSuccess}
+              onError={handleLoginError}
+            />
+          </GoogleOAuthProvider>
+        </div>
       )}
     </>
   );
