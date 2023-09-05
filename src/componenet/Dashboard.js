9a98b177e946951b18b35c7e542bd48a988ea7fd
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-09-05 17:42:06 +0530

    Update Dashboard.js

--- src/componenet/Dashboard.js
+++ src/componenet/Dashboard.js
@@ -1,9 +1,10 @@
-import React, { useEffect ,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import Nav from './Nav.js'
+import Nav from './Nav.js';
 
 export default function Dashboard() {
-  const [cred,setCred]=useState()
+  const [cred, setCred] = useState(null); // Initialize cred as null for loading handling
+
   useEffect(() => {
     // Declare an async function to fetch data
     const fetchData = async () => {
@@ -11,9 +12,10 @@ export default function Dashboard() {
         const response = await fetch(`https://vast-rose-piranha-vest.cyclic.app/api/user/dashboard/${Cookies.get("email")}`);
         const data = await response.json();
         console.log(data);
-        setCred(data)
+        setCred(data);
       } catch (error) {
         console.error('Error fetching data:', error);
+        setCred(null); // Set cred to null in case of an error
       }
     };
 
@@ -26,22 +28,27 @@ export default function Dashboard() {
   }, []); // Pass an empty dependency array to run the effect only once on mount
 
   // Redirect if the email cookie is not present
-  if (Cookies.get("email") === undefined) {
+  if (!Cookies.get("email")) {
     window.location.href = '/login';
     return null; // Return null to prevent rendering anything before redirect
   }
 
+  // Handle loading state
+  if (cred === null) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-    <Nav/>
-    <p>in das</p>
+      <Nav />
+      <p>in das</p>
       <h1>Dashboard</h1>
       <div className='cont'>
-      <img src={cred.profilePicture}></img>
-      <p>{cred.username}</p>
-      <p>{cred.email}</p>
-
+        <img src={cred.profilePicture} alt="Profile" />
+        <p>{cred.username}</p>
+        <p>{cred.email}</p>
       </div>
     </>
   ); 
 }
+
