eeb8564d734eea514a294fed5c7991abde873b5a
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-09-05 17:40:26 +0530

    Update Newuser.js

--- src/componenet/Newuser.js
+++ src/componenet/Newuser.js
@@ -1,6 +1,7 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import "./newuser.css";
 import Dashboard from "./Dashboard.js";
+import { useHistory } from "react-router-dom";
 
 export default function Newuser(props) {
   const [input, setInput] = useState("");
@@ -8,13 +9,13 @@ export default function Newuser(props) {
   const [pro, setPro] = useState(props.profile.join(","));
   const [available, setAvailable] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [entered,setEntered]=useState()
-  const [submitted,setSubmitted]=useState(false)
+  const [submitted, setSubmitted] = useState(false);
+  const history = useHistory();
 
-  
-  const handelChange = async (e) => {
+  const handleChange = async (e) => {
     setInput(e.target.value);
-    setLoading(true); // Show the loading indicator
+    setLoading(true);
+
     try {
       const response = await fetch(
         `https://vast-rose-piranha-vest.cyclic.app/api/user/check-username/${e.target.value}`
@@ -24,51 +25,69 @@ export default function Newuser(props) {
       setAvailable(data.available);
     } catch (error) {
       console.error("Error checking username availability:", error);
-      // Handle the error here if necessary
     } finally {
-      setLoading(false); // Hide the loading indicator
+      setLoading(false);
     }
   };
 
-  const handelSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const dataTosend = {
+    const dataToSend = {
       username: input,
       email: usermail,
       profilePicture: pro,
     };
-    const response = fetch(
+
+    const response = await fetch(
       `https://vast-rose-piranha-vest.cyclic.app/api/newuser`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(dataTosend),
+        body: JSON.stringify(dataToSend),
       }
     );
+
     console.log(response);
+
+    // Assuming submission is successful, set submitted to true
+    setSubmitted(true);
+
+    // Redirect to the dashboard
+    history.push("/dashboard");
   };
 
   return (
-    
     <>
-    {(submitted== true)?<Dashboard/>:}
-      <div className="bd">
-        <form onSubmit={handelSubmit}>
-          <label htmlFor="user">UserName</label>
-          <input type="text" id="user" value={input} onChange={handelChange}required/>
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            available === true ? (
-              <>{( input.length<1)?<p></p>:<p>username available</p>}</>
+      {submitted ? (
+        <Dashboard />
+      ) : (
+        <div className="bd">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="user">UserName</label>
+            <input
+              type="text"
+              id="user"
+              value={input}
+              onChange={handleChange}
+              required
+            />
+            {loading ? (
+              <p>Loading...</p>
             ) : (
-              <>{(input.length<1)?<p></p>:<p>username is already taken</p>}</>
-            )
-          )}
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+              <p>
+                {input.length < 1
+                  ? ""
+                  : available === true
+                  ? "Username available"
+                  : "Username is already taken"}
+              </p>
+            )}
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      )}
     </>
   );
-}
+    }
+    
