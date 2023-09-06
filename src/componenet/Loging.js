d9762957e35b83e456e212aa241757a174425c16
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-09-06 18:51:06 +0530

    Update Loging.js

--- src/componenet/Loging.js
+++ src/componenet/Loging.js
@@ -3,20 +3,23 @@ import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import Cookies from "js-cookie";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
-import Newuser from "./Newuser"; // Import Newuser component
-import Dashboard from "./Dashboard"; // Import Dashboard component
+import Newuser from "./Newuser";
+import Dashboard from "./Dashboard";
 import Nav from './Nav.js';
+import Loader from './Loader'; // Import or create your Loader component
 import "./login.css";
 
 export default function Loging() {
   const [email, setEmail] = useState("not");
-  const [profileUrl, setProfileUrl] = useState(""); // State to store profile URL
+  const [profileUrl, setProfileUrl] = useState("");
   const [showNewUser, setShowNewUser] = useState(false);
+  const [loading, setLoading] = useState(false); // Add loading state
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchdata = async () => {
       if (email !== "not") {
+        setLoading(true); // Set loading to true while fetching data
         var response = await fetch(
           `https://vast-rose-piranha-vest.cyclic.app/api/user/check-existence/${email}`
         );
@@ -24,14 +27,12 @@ export default function Loging() {
         console.log(data.exists);
         console.log(email, "return email");
         if (data.exists) {
-        
-          Cookies.set("email", email, { expires: 30}); 
-          
+          Cookies.set("email", email, { expires: 30 });
           navigate("/dashboard");
         } else {
-          // User doesn't exist, show the Newuser component
           setShowNewUser(true);
         }
+        setLoading(false); // Set loading back to false after data is fetched
       }
     };
     fetchdata();
@@ -40,7 +41,7 @@ export default function Loging() {
   const handleLoginSuccess = (credentialResponse) => {
     var decoded = jwt_decode(credentialResponse.credential);
     setEmail(decoded.email);
-    setProfileUrl(decoded.picture); // Set the profile URL from the decoded response
+    setProfileUrl(decoded.picture);
     console.log("ttt", decoded.email, decoded.picture);
   };
 
@@ -48,10 +49,8 @@ export default function Loging() {
     console.log("Login Failed");
   };
 
-  // Check if the email cookie is set
   const isEmailCookieSet = Cookies.get("email");
 
-  // If the email cookie is set, navigate to /dashboard
   useEffect(() => {
     if (isEmailCookieSet) {
       navigate("/dashboard");
@@ -60,30 +59,30 @@ export default function Loging() {
 
   return (
     <>
-      {isEmailCookieSet ? (
-        // Render Dashboard component if email cookie is set
+      {loading ? (
+        // Render a loader component while loading
+        <Loader />
+      ) : isEmailCookieSet ? (
         <Dashboard />
       ) : showNewUser ? (
-        // Render Newuser component with email and profile as props
         <Newuser email={email} profile={profileUrl} />
       ) : (
         <>
-        <Nav/>
-    <div className="main-login">
-        <div className="login-btn">
-    <h2>Welcome to, ExhibitMe</h2>
-    <p>Continue with google</p>
-          <GoogleOAuthProvider clientId="268873119322-g9kj6sj7fb8dmbs2mnj2r14gnk719md0.apps.googleusercontent.com">
-            <GoogleLogin
-              onSuccess={handleLoginSuccess}
-              onError={handleLoginError}
-            />
-          </GoogleOAuthProvider>
-        </div>
-    </div>
-    </>
+          <Nav />
+          <div className="main-login">
+            <div className="login-btn">
+              <h2>Welcome to, ExhibitMe</h2>
+              <p>Continue with google</p>
+              <GoogleOAuthProvider clientId="268873119322-g9kj6sj7fb8dmbs2mnj2r14gnk719md0.apps.googleusercontent.com">
+                <GoogleLogin
+                  onSuccess={handleLoginSuccess}
+                  onError={handleLoginError}
+                />
+              </GoogleOAuthProvider>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
-             }
-       
+}
