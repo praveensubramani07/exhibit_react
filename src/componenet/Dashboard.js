ac009aba3918c412d6bfb0a5af7591e8457d3dd7
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-10-15 16:58:52 +0530

    Update Dashboard.js

--- src/componenet/Dashboard.js
+++ src/componenet/Dashboard.js
@@ -1,42 +1,83 @@
 import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import Nav from './Nav.js';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate for redirection
-import Loader from './Loader'; // Import Loader component
+import { useNavigate } from 'react-router-dom';
+import Loader from './Loader';
 
 export default function Dashboard() {
-  const [cred, setCred] = useState(null); // Initialize cred as null for loading handling
-  const navigate = useNavigate(); // Initialize navigate function
+  const [cred, setCred] = useState(null);
+  const [bio, setBio] = useState('');
+  const [socialLinks, setSocialLinks] = useState([]);
+  const [otherLinks, setOtherLinks] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Declare an async function to fetch data
     const fetchData = async () => {
+      // ... (your existing fetch code)
+      const fetchData = async () => {
       try {
         const response = await fetch(`https://vast-rose-piranha-vest.cyclic.app/api/user/dashboard/${Cookies.get("email")}`);
         const data = await response.json();
         console.log(data);
-        setCred(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setCred(null); // Set cred to null in case of an error
-      }
-    };
+        
 
     fetchData(); // Call the function to fetch data
+      
 
-    // Return a cleanup function to cancel any pending requests
-    return () => {
-      // Add any necessary cleanup code here (if needed)
+      setCred(data);
+      setBio(data.bio || '');
+      setSocialLinks(data.socialLinks || []);
+      setOtherLinks(data.otherLinks || []);
     };
-  }, []); // Pass an empty dependency array to run the effect only once on mount
+    fetchData();
+  }, []);
 
-  // Redirect to login if the email cookie is not present
   if (!Cookies.get("email")) {
-    navigate('/login'); // Use navigate to redirect to login
-    return null; // Return null to prevent rendering anything before redirect
+    navigate('/login');
+    return null;
   }
 
-  // Handle loading state using Loader component
+  const handleBioChange = (e) => {
+    setBio(e.target.value);
+  };
+
+  const handleSocialLinkChange = (index, e) => {
+    const updatedSocialLinks = [...socialLinks];
+    updatedSocialLinks[index] = e.target.value;
+    setSocialLinks(updatedSocialLinks);
+  };
+
+  const handleOtherLinkChange = (index, e) => {
+    const updatedOtherLinks = [...otherLinks];
+    updatedOtherLinks[index] = e.target.value;
+    setOtherLinks(updatedOtherLinks);
+  };
+
+  const saveChanges = async () => {
+    // Send the updated bio, socialLinks, and otherLinks to the backend
+    const updatedCred = {
+      ...cred,
+      bio,
+      socialLinks,
+      otherLinks,
+    };
+
+    try {
+      await fetch(`https://vast-rose-piranha-vest.cyclic.app/api/user/${cred.username}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedCred),
+      });
+
+      // Optionally, you can set a success message or handle the response.
+      // Redirect to another page, display a success message, etc.
+    } catch (error) {
+      console.error('Error updating data:', error);
+    }
+  };
+
   if (cred === null) {
     return <Loader />;
   }
@@ -50,7 +91,34 @@ export default function Dashboard() {
         <img src={cred.profilePicture} alt="Profile" />
         <p>{cred.username}</p>
         <p>{cred.email}</p>
+
+        {/* Edit Bio */}
+        <textarea value={bio} onChange={handleBioChange} />
+        <button onClick={saveChanges}>Save Bio</button>
+
+        {/* Edit Social Links */}
+        {socialLinks.map((link, index) => (
+          <div key={index}>
+            <input
+              value={link}
+              onChange={(e) => handleSocialLinkChange(index, e)}
+            />
+          </div>
+        ))}
+        <button onClick={() => setSocialLinks([...socialLinks, ''])}>Add Social Link</button>
+
+        {/* Edit Other Links */}
+        {otherLinks.map((link, index) => (
+          <div key={index}>
+            <input
+              value={link}
+              onChange={(e) => handleOtherLinkChange(index, e)}
+            />
+          </div>
+        ))}
+        <button onClick={() => setOtherLinks([...otherLinks, ''])}>Add Other Link</button>
+        <button onClick={saveChanges}>Save Links</button>
       </div>
     </>
-  ); 
+  );
 }
