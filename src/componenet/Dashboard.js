e68be4ccafa9822e47ffb7538522231e026f664a
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-10-30 17:34:59 +0530

    Update Dashboard.js

--- src/componenet/Dashboard.js
+++ src/componenet/Dashboard.js
@@ -5,67 +5,57 @@ import { useNavigate } from 'react-router-dom';
 import Loader from './Loader';
 
 export default function Dashboard() {
-  const [cred, setCred] = useState(null);
-  const [bio, setBio] = useState('');
-  const [socialLinks, setSocialLinks] = useState([]);
-  const [otherLinks, setOtherLinks] = useState([]);
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
-      // ... (your existing fetch code)
-      
-        const response = await fetch(`https://vast-rose-piranha-vest.cyclic.app/api/user/dashboard/${Cookies.get("email")}`);
-        const data = await response.json();
-        console.log(data);
-
-      setCred(data);
-      setBio(data.bio || '');
-      setSocialLinks(data.socialLinks || []);
-      setOtherLinks(data.otherLinks || []);
+      if (!Cookies.get("email")) {
+        navigate('/login');
+      } else {
+        try {
+          const response = await fetch(`https://vast-rose-piranha-vest.cyclic.app/api/user/dashboard/${Cookies.get("email")}`);
+          if (response.ok) {
+            const data = await response.json();
+            setUser(data);
+          } else {
+            // Handle the error, e.g., redirect to an error page
+            navigate('/error');
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
     };
-    fetchData();
-  }, []);
 
-  if (!Cookies.get("email")) {
-    navigate('/login');
-    return null;
-  }
+    fetchData();
+  }, [navigate]);
 
   const handleBioChange = (e) => {
-    setBio(e.target.value);
+    setUser({ ...user, bio: e.target.value });
   };
 
   const handleSocialLinkChange = (index, e) => {
-    const updatedSocialLinks = [...socialLinks];
-    updatedSocialLinks[index] = e.target.value;
-    setSocialLinks(updatedSocialLinks);
+    const updatedSocialLinks = [...user.socialLinks];
+    updatedSocialLinks[index].url = e.target.value;
+    setUser({ ...user, socialLinks: updatedSocialLinks });
   };
 
   const handleOtherLinkChange = (index, e) => {
-    const updatedOtherLinks = [...otherLinks];
-    updatedOtherLinks[index] = e.target.value;
-    setOtherLinks(updatedOtherLinks);
+    const updatedOtherLinks = [...user.otherLinks];
+    updatedOtherLinks[index].url = e.target.value;
+    setUser({ ...user, otherLinks: updatedOtherLinks });
   };
 
   const saveChanges = async () => {
-    // Send the updated bio, socialLinks, and otherLinks to the backend
-    const updatedCred = {
-      ...cred,
-      bio,
-      socialLinks,
-      otherLinks,
-    };
-
     try {
       await fetch(`https://vast-rose-piranha-vest.cyclic.app/api/user/`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(updatedCred),
+        body: JSON.stringify(user),
       });
-
       // Optionally, you can set a success message or handle the response.
       // Redirect to another page, display a success message, etc.
     } catch (error) {
@@ -73,7 +63,7 @@ export default function Dashboard() {
     }
   };
 
-  if (cred === null) {
+  if (user === null) {
     return <Loader />;
   }
 
@@ -83,35 +73,35 @@ export default function Dashboard() {
       <p>in das</p>
       <h1>Dashboard</h1>
       <div className='cont'>
-        <img src={cred.profilePicture} alt="Profile" />
-        <p>{cred.username}</p>
-        <p>{cred.email}</p>
+        <img src={user.profilePicture} alt="Profile" />
+        <p>{user.username}</p>
+        <p>{user.email}
 
         {/* Edit Bio */}
-        <textarea value={bio} onChange={handleBioChange} />
+        <textarea value={user.bio} onChange={handleBioChange} />
         <button onClick={saveChanges}>Save Bio</button>
 
         {/* Edit Social Links */}
-        {socialLinks.map((link, index) => (
+        {user.socialLinks.map((link, index) => (
           <div key={index}>
             <input
-              value={link}
+              value={link.url}
               onChange={(e) => handleSocialLinkChange(index, e)}
             />
           </div>
         ))}
-        <button onClick={() => setSocialLinks([...socialLinks, ''])}>Add Social Link</button>
+        <button onClick={() => setUser({ ...user, socialLinks: [...user.socialLinks, { platform: '', url: '' }])}>Add Social Link</button>
 
         {/* Edit Other Links */}
-        {otherLinks.map((link, index) => (
+        {user.otherLinks.map((link, index) => (
           <div key={index}>
             <input
-              value={link}
+              value={link.url}
               onChange={(e) => handleOtherLinkChange(index, e)}
             />
           </div>
         ))}
-        <button onClick={() => setOtherLinks([...otherLinks, ''])}>Add Other Link</button>
+        <button onClick={() => setUser({ ...user, otherLinks: [...user.otherLinks, { title: '', url: '' }])}>Add Other Link</button>
         <button onClick={saveChanges}>Save Links</button>
       </div>
     </>
