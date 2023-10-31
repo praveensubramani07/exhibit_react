f8210f483fe3219807e63119805ce620a8a0450e
Praveen S <84556337+praveensubramani07@users.noreply.github.com>
2023-10-31 01:48:35 -1000

    Update Dashboard.js

--- src/componenet/Dashboard.js
+++ src/componenet/Dashboard.js
@@ -18,6 +18,7 @@ export default function Dashboard() {
           if (response.ok) {
             const data = await response.json();
             setUser(data);
+            console.log(user);
           } else {
             navigate('/error');
           }
